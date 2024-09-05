2d4afc1fdd9960e9511317fa8f79bec9aaeb61dd
unknown <jainprachii1907@gmail.com>
2024-09-05 14:08:31 +0530

    first commit

---
+++ pr1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int num, i, count=0;
+    printf("Enter Number:-");
+    scanf("%d", &num);
+
+    for(i=7; num!=0; i++);
+    {
+
+        printf("\nnum%d",num);
+        num = num/10;
+        count ++;
+}
+printf("\n number of count:- %d",count);
+
+}
